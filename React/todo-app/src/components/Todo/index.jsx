107698a77692866d846dec511d55dbc0c49984cd
glaborde <glaborde@ANDAR-190>
2025-09-08 16:33:29 -0300

    se agregó el boton para editar el nombre de la tarea

--- React/todo-app/src/components/Todo/index.jsx
+++ React/todo-app/src/components/Todo/index.jsx
@@ -1,13 +1,15 @@
 /** Components */
 import Button from '../Button'
 
-const Todo = ({ todo, onclickRemove, onToogle }) => {
+const Todo = ({ todo, onclickRemove, onToogle,onEditTitle }) => {
   
   return(
     <li className="todo-container">
       <input type="checkbox" checked={todo.completed} onChange={() => onToogle(todo.id)} />
       <span>{todo.title}</span>
       <Button text={"X"} className={"todo-delete"} callBack={() => onclickRemove(todo.id)} />
+        <Button text={"Editar nombre"} className={"todo-edit"} callBack={() => onEditTitle(todo.id)}
+/>
     </li>
   )
 }
