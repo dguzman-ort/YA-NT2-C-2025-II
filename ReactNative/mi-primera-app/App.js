78e19ccfb220551bc8d4adbef2284fabcee27ea7
Daniel Guzman <dguzman1012@gmail.com>
2025-10-21 21:19:38 -0300

    Mi primera App - ReactNative

---
+++ ReactNative/mi-primera-app/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hola grupo NT2</Text>
+      <Button title='Presiona aqui!' onPress={() => alert('Hola grupo NT2')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
